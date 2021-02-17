056bb4fe73c5d4237530afc38b82e12900a85c09
leandoerbore <rusa8612331@gmail.com>
2021-02-17 12:26:20 +0300

    Add test line

--- FirstProjectTest/src/com/company/Main.java
+++ FirstProjectTest/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+
     }
 }
